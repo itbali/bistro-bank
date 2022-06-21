67309b7f661e0d64f18f392d242ffacea4f40182
dyuzhev <alex.itbali@gmail.com>
2022-06-21 20:52:35 +0300

    Assets, Metadata, and CSS in Next

---
+++ pages/posts/first-post.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Head from 'next/head';
+
+
+const FirstPost = () => {
+	return (
+		<>
+
+			<Head>
+				<title>First Post</title>
+			</Head>
+
+			<h1>First post</h1>
+			<h2>
+				<Link href={'/'}>
+					<a>Back home</a>
+				</Link>
+			</h2>
+		</>
+	);
+};
+export default FirstPost
