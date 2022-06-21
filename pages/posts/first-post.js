1266aca5468160bc244288f78b41f96ec1f2dd27
dyuzhev <alex.itbali@gmail.com>
2022-06-21 20:58:28 +0300

    Script tag in next

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from 'next/head';
+import Script from 'next/script';
 
 
 const FirstPost = () => {
@@ -9,6 +10,14 @@ const FirstPost = () => {
 			<Head>
 				<title>First Post</title>
 			</Head>
+
+			<Script
+				src="https://connect.facebook.net/en_US/sdk.js"
+				strategy="lazyOnload"
+				onLoad={() =>
+					console.log(`script loaded correctly, window.FB has been populated`)
+				}
+			/>
 
 			<h1>First post</h1>
 			<h2>
